82c28002dcc3d809b8181f95ef9e19744562a6dd
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-19 23:45:52 +0200

    create models to add data to db

---
+++ SMartS/models.py
@@ -0,0 +1,13 @@
+from SMartS import db
+
+
+class Message(db.Model):
+    __tablename__ = 'messages'
+    id = db.Column(db.Integer, primary_key=True)
+    receiver = db.Column(db.String(50), nullable=True)
+    date = db.Column(db.Date, nullable=True)
+    text = db.Column(db.String(160), nullable=True)
+    sender = db.Column(db.String(50), nullable=True)
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__}>: Message with ID {self.id} posted on {self.date} by {self.sender} '
