78b0101c4ac2ce125c73ed41cf282d94377aa046
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-19 23:32:27 +0200

    add connection to database

--- SMartS/__init__.py
+++ SMartS/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+db = SQLAlchemy(app)
+
 
 from SMartS import messages
-
-
-@app.route('/')
-def index():
-    return 'Hello'
