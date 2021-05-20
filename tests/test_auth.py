859b40b7f27df7a948919a6e7b0b3a68281a5f64
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-20 16:02:59 +0200

    create test checking valid content type

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -33,3 +33,17 @@ def test_registration_invalid_data(client, data, missing_field):
     assert 'token' not in response_data
     assert missing_field in response_data['message']
     assert 'Missing data for required field.' in response_data['message'][missing_field]
+
+
+def test_registration_invalid_content_type(client):
+    response = client.post('/api/v1/auth/register',
+                           data={
+                               'username': 'test',
+                               'password': 'qwerty',
+                               'email': 'test@gmail.com'
+                           })
+    response_data = response.get_json()
+    assert response.status_code == 415
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert 'token' not in response_data
