74a0a5d13fb78ffadf7d2e78fab35735aef5a51c
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-20 12:51:29 +0200

    add models to create table  'users'

--- smarts/models.py
+++ smarts/models.py
@@ -15,6 +15,15 @@ class Message(db.Model):
         return f'<{self.__class__.__name__}>: Message posted on {self.date} by {self.sender} '
 
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False, unique=True, index=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 class MessageSchema(Schema):
     id = fields.Integer(dump_only=True)
     receiver = fields.String(required=True, validate=validate.Length(max=50))
