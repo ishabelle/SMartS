ca213f8fc4be6e66f5d05dbde6fc4386a4a7a8ed
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-20 09:49:26 +0200

    refactor

--- smarts/__init__.py
+++ smarts/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-from SMartS import messages
-from SMartS import models
+from smarts import messages
+from smarts import models
