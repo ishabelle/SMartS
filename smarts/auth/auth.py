9c6c82f8860169d30658bd23d09fa58b48cfe08b
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-20 13:46:28 +0200

    add login to user

--- smarts/auth/auth.py
+++ smarts/auth/auth.py
@@ -1,7 +1,7 @@
 from smarts.auth import auth_bp
 from smarts.utils import validate_json_content_type
 from webargs.flaskparser import use_args
-from smarts.models import User, user_schema
+from smarts.models import User, user_schema, UserSchema
 from flask import abort, jsonify
 from smarts import db
 
@@ -20,6 +20,25 @@ def register(args: dict):
 
     db.session.add(user)
     db.session.commit()
+
+    token = user.generate_jwt()
+
+    return jsonify({
+        'success': True,
+        'token': token.decode()
+    })
+
+
+@auth_bp.route('/login', methods=['POST'])
+@validate_json_content_type
+@use_args(UserSchema(only=['username', 'password']), error_status_code=400)
+def login(args: dict):
+    user = User.query.filter(User.username == args['username']).first()
+    if not user:
+        abort(401, description='Invalid credentials')
+
+    if not user.is_password_valid(args['password']):
+        abort(401, description='Invalid credentials')
 
     token = user.generate_jwt()
 
