e8628aa37bbb45c0deb522177617af1918df6019
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-20 12:39:45 +0200

    implement function create_app

--- smarts/__init__.py
+++ smarts/__init__.py
@@ -3,13 +3,23 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy()
+migrate = Migrate()
 
-from smarts import messages
-from smarts import models
-from smarts.commands import db_manage_commands
-from smarts import errors
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from smarts.commands import db_manage_bp
+    from smarts.errors import errors_bp
+    from smarts.messages import messages_bp
+    app.register_blueprint(db_manage_bp)
+    app.register_blueprint(errors_bp)
+    app.register_blueprint(messages_bp, url_prefix='/api/v1')
+
+    return app
