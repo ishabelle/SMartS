2c148fe6648ab28dde3b830fc1d91677ddf40fd1
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-20 16:01:13 +0200

    create test checking valid data

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,3 +1,4 @@
+import pytest
 
 
 def test_registration(client):
@@ -12,3 +13,23 @@ def test_registration(client):
     assert response.headers['Content-Type'] == 'application/json'
     assert response_data['success'] is True
     assert response_data['token']
+
+
+@pytest.mark.parametrize(
+    'data,missing_field',
+    [
+        ({'username': 'test', 'password': 'qwerty'}, 'email'),
+        ({'username': 'test', 'email': 'test@gmail.com'}, 'password'),
+        ({'password': 'qwerty', 'email': 'test@gmail.com'}, 'username')
+    ]
+)
+def test_registration_invalid_data(client, data, missing_field):
+    response = client.post('/api/v1/auth/register',
+                           json=data)
+    response_data = response.get_json()
+    assert response.status_code == 400
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert 'token' not in response_data
+    assert missing_field in response_data['message']
+    assert 'Missing data for required field.' in response_data['message'][missing_field]
