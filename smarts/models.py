18fae5746845d86d92a96ecd380e40f728c5bf2d
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-20 11:20:34 +0200

    add data validation

--- smarts/models.py
+++ smarts/models.py
@@ -1,25 +1,31 @@
 from smarts import db
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate, validates, ValidationError
+from datetime import datetime
 
 
 class Message(db.Model):
     __tablename__ = 'messages'
     id = db.Column(db.Integer, primary_key=True)
-    receiver = db.Column(db.String(50), nullable=True)
-    date = db.Column(db.Date, nullable=True)
-    text = db.Column(db.String(160), nullable=True)
-    sender = db.Column(db.String(50), nullable=True)
+    receiver = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    text = db.Column(db.String(160), nullable=False)
+    sender = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return f'<{self.__class__.__name__}>: Message posted on {self.date} by {self.sender} '
 
 
 class MessageSchema(Schema):
-    id = fields.Integer()
-    receiver = fields.String()
-    date = fields.Date('%d-%m-%Y')
-    text = fields.String()
-    sender = fields.String()
+    id = fields.Integer(dump_only=True)
+    receiver = fields.String(required=True, validate=validate.Length(max=50))
+    date = fields.Date('%d-%m-%Y', required=True)
+    text = fields.String(required=True, validate=validate.Length(min=1, max=160))
+    sender = fields.String(required=True, validate=validate.Length(max=50))
+
+    @validates('date')
+    def validate_date(self, value):
+        if value > datetime.now().date():
+            raise ValidationError(f'Date must be lower than {datetime.now().date()}')
 
 
 message_schema = MessageSchema()
