afe27d315e2ebe3a362a544330427ff9a10d8575
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-20 22:37:30 +0200

    refactor

--- smarts_app.py
+++ smarts_app.py
@@ -4,4 +4,11 @@ from flask import Flask
 app = create_app()
 
 if __name__ == "__main__":
+    app.debug = True
+    PORT = process.env.PORT | '8080'
     app.run()
+    app.run(
+        host='localhost',
+        port=PORT,
+        debug=True,
+    )
