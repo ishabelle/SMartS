b6db96c2d1d2f064d9547746ce8b49d506b8ef41
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-20 12:28:49 +0200

    add blueprint in file

--- smarts/messages/messages.py
+++ smarts/messages/messages.py
@@ -1,11 +1,12 @@
-from smarts import app, db
+from smarts import db
 from flask import jsonify, request
 from smarts.models import Message, MessageSchema, message_schema
 from webargs.flaskparser import use_args
 from smarts.utils import validate_json_content_type
+from smarts.messages import messages_bp
 
 
-@app.route('/api/v1/messages', methods=['GET'])
+@messages_bp.route('/api/v1/messages', methods=['GET'])
 def get_messages():
     messages = Message.query.all()
     message_schema = MessageSchema(many=True)
@@ -17,7 +18,7 @@ def get_messages():
     })
 
 
-@app.route('/api/v1/messages/<int:message_id>', methods=['GET'])
+@messages_bp.route('/api/v1/messages/<int:message_id>', methods=['GET'])
 def get_message(message_id: int):
     message = Message.query.get_or_404(message_id, description=f'Message with id {message_id} not found')
 
@@ -27,7 +28,7 @@ def get_message(message_id: int):
     })
 
 
-@app.route('/api/v1/messages', methods=['POST'])
+@messages_bp.route('/api/v1/messages', methods=['POST'])
 @validate_json_content_type
 @use_args(message_schema, error_status_code=400)
 def create_message(args: dict):
@@ -41,7 +42,7 @@ def create_message(args: dict):
     }), 201
 
 
-@app.route('/api/v1/messages/<int:message_id>', methods=['PUT'])
+@messages_bp.route('/api/v1/messages/<int:message_id>', methods=['PUT'])
 @validate_json_content_type
 @use_args(message_schema, error_status_code=400)
 def update_message(args: dict, message_id: int):
@@ -58,7 +59,7 @@ def update_message(args: dict, message_id: int):
     })
 
 
-@app.route('/api/v1/messages/<int:message_id>', methods=['DELETE'])
+@messages_bp.route('/api/v1/messages/<int:message_id>', methods=['DELETE'])
 def delete_message(message_id: int):
     message = Message.query.get_or_404(message_id, description=f'Message with id {message_id} not found')
     db.session.delete(message)
