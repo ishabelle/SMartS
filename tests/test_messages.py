0040789ae1d31e3513ea5179a534850b40f1d5fa
ishabelle <kolodzinska.izabela@gmail.com>
2021-05-20 16:40:35 +0200

    create test checking get all messages

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -10,11 +10,10 @@ def test_get_messages_no_records(client):
     assert response.get_json() == expected_result
 
 
-def test_get_authors(client, sample_data):
+def test_get_messages(client, sample_data):
     response = client.get('/api/v1/messages')
     response_data = response.get_json()
     assert response.status_code == 200
     assert response.headers['Content-Type'] == 'application/json'
     assert response_data['success'] is True
     assert len(response_data['data'])
-
